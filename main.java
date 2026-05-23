cf460a01667b308c03c443eb4515af4020ee7adf
www318 <w3185855109@outlook.com>
2026-05-23 18:38:48 +0800

    release-1.2.0

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 public class main {
 
     public static void main(String[] args) {
+        System.out.println("release-1.2.0");
         System.out.println("feature branch!!!");
-        System.out.println("123");
         System.out.println("Hello World!");
     }
 }
